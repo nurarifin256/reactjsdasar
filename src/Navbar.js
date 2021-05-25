cb34475e0f8efcc60dd5a86f6b001435d8be859e
nurarifin256 <arifinnur402@gmail.com>
2021-05-25 18:42:49 +0700

    belajar component dan export, import

---
+++ src/Navbar.js
@@ -0,0 +1,38 @@
+
+// function component
+// import React from 'react'
+
+// export default function Navbar() {
+//     return (
+//         <div>
+//             <h2>Navbar</h2>
+//         </div>
+//     )
+// }
+
+// class component
+// import React, { Component } from 'react'
+
+// export default class Navbar extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h2>Navbar</h2>
+//             </div>
+//         )
+//     }
+// }
+
+
+import React from 'react'
+
+export const Navbar = () => {
+    return (
+        <div>
+            <h2>Navbar</h2>
+        </div>
+    )
+}
+
+export default Navbar;
+
